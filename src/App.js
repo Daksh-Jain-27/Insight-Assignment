216a9041685f4ce9baa434cfd4c122f30c2c6042
Daksh-Jain-27 <djiitian27@gmail.com>
2025-04-07 02:08:17 +0530

    deploy

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./pages/Navbar";
 import Home from "./pages/Home";
 import Academics from "./pages/Academics";
